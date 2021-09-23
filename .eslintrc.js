c9c81538f81ee3c33ac950b77140eeb8a6f2b1b6
dmytroyevlakhov <yevlakhovdmytro@gmail.com>
2021-09-23 11:42:19 +0300

    disable react/self-closing-comp eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,16 @@ module.exports = {
     'plugin:react/recommended',
   ],
   rules: {
+    // React
     'react/prop-types': 0,
+    'react/self-closing-comp': 0,
+
+    // JavaScript
     semi: 0,
     'no-proto': 0,
     'no-unused-vars': 0,
+
+    // TypeScript
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
