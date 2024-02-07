d50b012528dfcb0c88ae189a88b3990894838ebb
mykhalenych <mykhalenych@gmail.com>
2024-02-07 13:30:50 +0200

    updated packages,changed files for template

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './index.scss';
+
 import { App } from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(<App />);
